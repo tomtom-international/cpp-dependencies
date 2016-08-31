6b7895e5564eca9630d13a140fd12b51d62d9ab3
Chernov Alexandr <chernov.aleksandre@gmail.com>
2016-08-31 13:28:01 +0300

    + Try make code of resolving of std/boost using easier to read.

--- src/FilesystemInclude.h
+++ src/FilesystemInclude.h
@@ -1,25 +1,10 @@
 #ifndef __DEP_CHECKER__FILESYSTEMINCLUDE_H
 #define __DEP_CHECKER__FILESYSTEMINCLUDE_H
 
-#ifdef _MSC_VER
-
-#if ( _MSC_VER < 1900 )
-
-#include "BoostFilesystem.h"
-
-#else
-
-#include "StdFilesystem.h"
-
-#endif // _msc version
-
-#endif // _MSC_VER
-
-#ifdef __GNUC__
-
-#if __GNUC__ > 5 || \
-    (__GNUC__ == 5 && \
-        (__GNUC_MINOR__ > 3 || (__GNUC_MINOR__ == 3 && __GNUC_PATCHLEVEL__ > 0)))
+#if ( defined _MSC_VER && ( _MSC_VER > 1900 || _MSC_VER == 1900 ) ) || \
+        ( defined __GNUC__ && (__GNUC__ > 5 || \
+                                (__GNUC__ == 5 && \
+                                    (__GNUC_MINOR__ > 3 || (__GNUC_MINOR__ == 3 && __GNUC_PATCHLEVEL__ > 0) ) ) ) )
 
 #include "StdFilesystem.h"
 
@@ -27,15 +12,6 @@
 
 #include "BoostFilesystem.h"
 
-#endif // gcc version
-
-#endif // __GNUC__
-
-#ifdef __clang__
-#define __clang__
-
-#include "BoostFilesystem.h"
-
-#endif // __clang__
+#endif
 
 #endif // !__DEP_CHECKER__FILESYSTEMINCLUDE_H
--- src/FstreamInclude.h
+++ src/FstreamInclude.h
@@ -1,25 +1,10 @@
 #ifndef __DEP_CHECKER__FSTREAMINCLUDE_H
 #define __DEP_CHECKER__FSTREAMINCLUDE_H
 
-#ifdef _MSC_VER
-
-#if ( _MSC_VER < 1900 )
-
-#include "BoostFstream.h"
-
-#else
-
-#include "StdFstream.h"
-
-#endif
-
-#endif // _MSC_VER
-
-#ifdef __GNUC__
-
-#if __GNUC__ > 5 || \
-    (__GNUC__ == 5 && \
-        (__GNUC_MINOR__ > 3 || (__GNUC_MINOR__ == 3 && __GNUC_PATCHLEVEL__ > 0)))
+#if ( defined _MSC_VER && ( _MSC_VER > 1900 || _MSC_VER == 1900 )  ) || \
+        ( defined __GNUC__ && (__GNUC__ > 5 || \
+                                (__GNUC__ == 5 && \
+                                    (__GNUC_MINOR__ > 3 || (__GNUC_MINOR__ == 3 && __GNUC_PATCHLEVEL__ > 0) ) ) ) )
 
 #include "StdFstream.h"
 
@@ -28,13 +13,5 @@
 #include "BoostFstream.h"
 
 #endif
-
-#endif // __GNUC__
-
-#ifdef __clang__
-
-#include "BoostFstream.h"
-
-#endif // __clang__
 
 #endif // !__DEP_CHECKER__FSTREAMINCLUDE_H
