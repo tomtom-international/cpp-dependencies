885acfb70c849344d56a01a4aa4c41c4b02e535d
Peter Bindels <peter.bindels@tomtom.com>
2016-11-02 11:56:38 +0100

    Use USE_BOOST define in headers instead of heuristic

--- src/FilesystemInclude.h
+++ src/FilesystemInclude.h
@@ -1,21 +1,18 @@
 #ifndef __DEP_CHECKER__FILESYSTEMINCLUDE_H
 #define __DEP_CHECKER__FILESYSTEMINCLUDE_H
 
-#if ( defined _MSC_VER && ( _MSC_VER > 1900 || _MSC_VER == 1900 ) ) || \
-        ( defined __GNUC__ && (__GNUC__ > 5 || \
-                                (__GNUC__ == 5 && \
-                                    (__GNUC_MINOR__ > 3 || (__GNUC_MINOR__ == 3 && __GNUC_PATCHLEVEL__ > 0) ) ) ) )
-
-#include <experimental/filesystem>
-
-namespace filesystem = std::experimental::filesystem;
-
-#else
+#ifdef USE_BOOST
 
 #include <boost/filesystem.hpp>
 
 namespace filesystem = boost::filesystem;
 
+#else
+
+#include <experimental/filesystem>
+
+namespace filesystem = std::experimental::filesystem;
+
 #endif
 
-#endif // !__DEP_CHECKER__FILESYSTEMINCLUDE_H
+#endif
