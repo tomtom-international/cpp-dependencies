4f77927dd6eb121ee2bf644373275d840fae3011
U-HWIT\jaques.arantes <jaques.arantes@HWITNTBK-370.hwit.com.br>
2025-07-31 15:59:23 -0300

    Updated project to build wwith C++20 and std::filesystem

--- src/FilesystemInclude.h
+++ src/FilesystemInclude.h
@@ -3,15 +3,26 @@
 
 #ifdef WITH_BOOST
 
-#include <boost/filesystem.hpp>
-
-namespace filesystem = boost::filesystem;
+    #include <boost/filesystem.hpp>
+    namespace filesystem = boost::filesystem;
 
 #else
+#   if __cplusplus >= 201703L
+    #include <filesystem>
+    namespace filesystem = std::filesystem;
 
-#include <experimental/filesystem>
+    // Code specific to C++17 or later
+    #define HAS_CPP17_FEATURES
+#   elif __cplusplus >= 201103L
+    #include <experimental/filesystem>
+    namespace filesystem = std::experimental::filesystem;
 
-namespace filesystem = std::experimental::filesystem;
+    // Code specific to C++11 or later (but not C++17)
+    #define HAS_CPP11_FEATURES
+#   else
+    // Code for older C++ versions (e.g., C++98/03)
+    #pragma error("C++ version is too old, please use C++11 or later.")
+#   endif
 
 #endif
 
